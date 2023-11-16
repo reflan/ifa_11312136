8dfbdc872daa122e9ad0946b9037295885126481
reflan nuari <reflannuari@gmail.com>
2023-11-16 09:07:49 +0700

    commit

---
+++ android/app/src/main/kotlin/com/example/ifa_11312136/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ifa_11312136
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:ifa_11312136/firebase_options.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(GetMaterialApp(
+    title: "Aplication",
+    initialRoute: AppPages.INITIAL,
+    getPages: AppPages.routes,
+  ));
+}
